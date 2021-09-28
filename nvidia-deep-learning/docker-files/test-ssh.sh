1bc18387322245c92d81b339b938f3760489aa52
xsthunder <xsthunder@outlook.com>
2021-09-28 10:02:29 +0800

    add robust to test-ssh.sh

--- nvidia-deep-learning/docker-files/test-ssh.sh
+++ nvidia-deep-learning/docker-files/test-ssh.sh
@@ -4,5 +4,5 @@ set -e
 # use password
 
 #exit 1 && echo 'root:' | chpasswd
-#sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
-#sed -i "s/PasswordAuthentication no/#PasswordAuthentication yes/" /etc/ssh/sshd_config
+#sed -i "s/#\?PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
+#sed -i "s/#\?PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
